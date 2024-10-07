7bb7fd38aacf02df07c4d383968c85568028c037
VaclavK <vaclav@vaclavkabat.net>
2024-10-07 09:53:53 +0100

    Add ServiceBus topic creation to ResourceInitializerService
    Introduced ServiceBusAdministrationClient to the constructor of
    ResourceInitializerService. Updated InitializeResources method
    to create a Service Bus topic asynchronously using the new
    client and configuration settings. Existing blob container
    creation functionality remains unchanged.

--- src/SapAct/Services/ResourceInitializerService.cs
+++ src/SapAct/Services/ResourceInitializerService.cs
@@ -1,9 +1,11 @@
 ﻿namespace SapAct.Services;
 
-public class ResourceInitializerService(BlobServiceClient blobServiceClient, IConfiguration configuration)
+public class ResourceInitializerService(BlobServiceClient blobServiceClient, ServiceBusAdministrationClient serviceBusAdministrationClient,  IConfiguration configuration)
 {
 	public async Task InitializeResources()
 	{
+		await serviceBusAdministrationClient.CreateTopicAsync(configuration.GetServiceBusTopicName());
+		//SB subscriptions created per subscription/worker
 		await blobServiceClient.GetBlobContainerClient(configuration.GetLockServiceBlobContainerNameOrDefault()).CreateIfNotExistsAsync();
 	}
 }
