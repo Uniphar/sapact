ee006d52be89d839a4d405ab4051ff8a72a9feb8
Daniël Koek <contact@danielkoek.net>
2026-02-20 13:28:55 +0000

    Update LogAnalyticsService.cs

--- src/SapAct/Services/LogAnalyticsService.cs
+++ src/SapAct/Services/LogAnalyticsService.cs
@@ -1,4 +1,6 @@
-﻿namespace SapAct.Services;
+﻿using System.Text.RegularExpressions;
+
+namespace SapAct.Services;
 
 public class LogAnalyticsService(
     LogAnalyticsServiceConfiguration configuration,
@@ -15,7 +17,10 @@ public class LogAnalyticsService(
     private string WorkspaceResourceId => $"/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}";
 
     private string GetDCRUrl(string tableName) => $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.Insights/dataCollectionRules/{tableName}DCR?api-version=2023-03-11";
-    private string GetTableUrl(string tableName) => $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}/tables/{tableName}_CL?api-version=2022-10-01";
+    private string GetTableUrl(string tableName)
+    {
+        return !Regex.IsMatch(tableName, "^[A-Za-z0-9-_]+$") ? throw new ArgumentException("Invalid table name.", nameof(tableName)) : $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}/tables/{tableName}_CL?api-version@2025-07-01";
+    }
 
     private async Task<string> SyncTableSchema(string tableName, JsonElement payload, CancellationToken cancellationToken)
     {
