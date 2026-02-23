13b7046adc728f8b588b183de891bcab2ad46669
Daniël Koek <contact@danielkoek.net>
2026-02-23 10:07:11 +0000

    Create table sanatize!

--- src/SapAct/Services/LogAnalyticsService.cs
+++ src/SapAct/Services/LogAnalyticsService.cs
@@ -18,7 +18,7 @@ public class LogAnalyticsService(
     private string WorkspaceResourceId => $"/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}";
 
     private string GetDCRUrl(string tableName) => $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.Insights/dataCollectionRules/{tableName}DCR?api-version=2024-03-11";
-    private string GetTableUrl(string tableName) => !Regex.IsMatch(tableName, "^[A-Za-z0-9-_]+$") ? throw new ArgumentException("Invalid table name.", nameof(tableName)) : $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}/tables/{tableName}_CL?api-version=2025-07-01";
+    private string GetTableUrl(string tableName) => $"https://management.azure.com/subscriptions/{configuration.SubscriptionId}/resourceGroups/{configuration.ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{configuration.WorkspaceName}/tables/{tableName}_CL?api-version=2025-07-01";
 
     private async Task<string> SyncTableSchema(string tableName, JsonElement payload, CancellationToken cancellationToken)
     {
@@ -119,7 +119,8 @@ public class LogAnalyticsService(
         var dcrId = "";
         //get key properties
         var messageProperties = ExtractMessageRootProperties(payload);
-        var objectType = messageProperties?.objectType ?? topic;
+        //will be used for the table name, cleanup up so it will work for the table
+        var objectType = (messageProperties?.objectType ?? topic).MakeTableFriendly();
         var dataVersion = messageProperties?.dataVersion ?? "1";
         if (Consts.DeltaEventType == messageProperties?.eventType) return;
 
